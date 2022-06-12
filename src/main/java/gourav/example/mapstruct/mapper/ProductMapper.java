8811fe7068b76358d682ecb86670974f80bbdb96
Gourav <gourav2824@gmail.com>
2022-06-13 00:58:42 +0530

    Refactor: remove the comments from Mapper

--- src/main/java/gourav/example/mapstruct/mapper/ProductMapper.java
+++ src/main/java/gourav/example/mapstruct/mapper/ProductMapper.java
@@ -1,6 +1,5 @@
 package gourav.example.mapstruct.mapper;
 
-import gourav.example.mapstruct.model.City;
 import gourav.example.mapstruct.model.Price;
 import gourav.example.mapstruct.model.PriceDTO;
 import gourav.example.mapstruct.model.Product;
@@ -12,21 +11,16 @@ import org.mapstruct.ReportingPolicy;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface ProductMapper {
+
     @Mapping(target = "status", ignore = true)
-//    @Mapping(target = "price.countryCode", source = "city.countryCode")
     @Mapping(target = "color", source = "colour")
     @Mapping(target = ".", source = "weight")
     @Mapping(target = "country", source = "city.country")
     @Mapping(target = "size.dimensions", source = "productDimensions")
     ProductDTO productToProductDTO(Product product);
 
-//    @Mapping(target = "countryCode", source = ".city.countryCode")
     @Mapping(target = "countryCode", ignore = true)
     PriceDTO priceToPriceDTO(Price price);
-
-//    @Mapping(target = "amount", ignore = true)
-//    @Mapping(target = "currency", ignore = true)
-//    PriceDTO cityToPriceDTO(City city);
 
     @Mapping(target = "code", ignore = true)
     @Mapping(target = "productType", source = ".")
