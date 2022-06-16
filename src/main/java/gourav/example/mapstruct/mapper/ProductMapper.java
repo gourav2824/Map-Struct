56a8a510fbb5db1d8c39d96de49ef606516fc22c
Gourav <gourav2824@gmail.com>
2022-06-17 00:54:01 +0530

    Add: add mapping for Status enum

--- src/main/java/gourav/example/mapstruct/mapper/ProductMapper.java
+++ src/main/java/gourav/example/mapstruct/mapper/ProductMapper.java
@@ -4,15 +4,17 @@ import gourav.example.mapstruct.model.Price;
 import gourav.example.mapstruct.model.PriceDTO;
 import gourav.example.mapstruct.model.Product;
 import gourav.example.mapstruct.model.ProductDTO;
+import gourav.example.mapstruct.model.ProductStatus;
+import gourav.example.mapstruct.model.Status;
 import gourav.example.mapstruct.model.Type;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
+import org.mapstruct.ValueMapping;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface ProductMapper {
 
-    @Mapping(target = "status", ignore = true)
     @Mapping(target = "color", source = "colour")
     @Mapping(target = ".", source = "weight")
     @Mapping(target = "country", source = "city.country")
@@ -25,4 +27,7 @@ public interface ProductMapper {
     @Mapping(target = "code", ignore = true)
     @Mapping(target = "productType", source = ".")
     Type typeToProductType(String type);
+
+    @ValueMapping(source = "UNDEFINED", target = "UNKNOWN")
+    ProductStatus statusToProductStatus(Status status);
 }
