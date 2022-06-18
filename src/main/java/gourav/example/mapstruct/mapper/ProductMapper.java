ebd85dc1d59f08a1f1ff599d0c7dfa3c10fd7996
Gourav <gourav2824@gmail.com>
2022-06-19 00:46:56 +0530

    Refactor: map PriceDTO from Price & City using Simple Mapping

--- src/main/java/gourav/example/mapstruct/mapper/ProductMapper.java
+++ src/main/java/gourav/example/mapstruct/mapper/ProductMapper.java
@@ -1,16 +1,11 @@
 package gourav.example.mapstruct.mapper;
 
-import gourav.example.mapstruct.model.City;
-import gourav.example.mapstruct.model.Price;
-import gourav.example.mapstruct.model.PriceDTO;
 import gourav.example.mapstruct.model.Product;
 import gourav.example.mapstruct.model.ProductDTO;
 import gourav.example.mapstruct.model.ProductStatus;
 import gourav.example.mapstruct.model.Status;
-import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.ValueMapping;
 
@@ -23,16 +18,11 @@ public interface ProductMapper {
     @Mapping(target = "size.dimensions", source = "productDimensions")
     @Mapping(target = "type.productType", source = "type")
     @Mapping(target = "productCode", expression = "java(product.getId() + product.getName() + product.getProductCode())")
-    @Mapping(target = "price", ignore = true)
+    @Mapping(target = "price.amount", source = "price.amount")
+    @Mapping(target = "price.currency", source = "price.currency")
+    @Mapping(target = "price.countryCode", source = "city.countryCode")
     ProductDTO productToProductDTO(Product product);
-
-    PriceDTO mapPriceDtoFromPriceAndCity(Price price, City city);
 
     @ValueMapping(source = "UNDEFINED", target = "UNKNOWN")
     ProductStatus statusToProductStatus(Status status);
-
-    @AfterMapping
-    default void setPriceDTO(@MappingTarget ProductDTO productDTO, Product product) {
-        productDTO.setPrice(mapPriceDtoFromPriceAndCity(product.getPrice(), product.getCity()));
-    }
 }
